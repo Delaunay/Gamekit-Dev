0901659239854a239dfed18cd08a06f052cad002
Setepenre <pierre.delaunay@outlook.com>
2022-06-01 19:47:12 -0400

    Add moving dummies for homing testing

--- Source/ChessyEditor.Target.cs
+++ Source/ChessyEditor.Target.cs
@@ -8,7 +8,8 @@ public class ChessyEditorTarget : TargetRules
 		Type = TargetType.Editor;
 		DefaultBuildSettings = BuildSettingsVersion.V2;
 		ExtraModuleNames.AddRange(new[] {
-			"Gamekit", 
+			"Gamekit",
+			"GamekitEd",
 			"Chessy", 
 			// "ScriptEditorPlugin" 
 		});
